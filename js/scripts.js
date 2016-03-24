eccdc8a9f759de781c454eca39810912da255560
Adam Craig and Kellie Olson <adam@>
2016-03-24 14:27:59 -0700

    add logic to determine triangle type based on input, and add some error catching

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,28 @@
 $(document).ready(function() {
-  $("form#dating").submit(function(event) {
-    var age = parseInt($("input#age").val());
-    var gender = $("select#gender").val();
+  $("form#triangle").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
 
-    if (age) {
-      if (age <= 30 && gender === 'female') {
-      $("#celeb1").show();
-      $("#celeb2").hide();
-      $("#celeb3").hide();
-      $("#celeb4").hide();
-    } else if (age > 30 && gender === 'female') {
-      $("#celeb2").show();
-      $("#celeb1").hide();
-      $("#celeb3").hide();
-      $("#celeb4").hide();
-    } else if (age <= 30 && gender === 'male') {
-      $("#celeb3").show();
-      $("#celeb1").hide();
-      $("#celeb2").hide();
-      $("#celeb4").hide();
-    } else {
-      $("#celeb4").show();
-      $("#celeb1").hide();
-      $("#celeb2").hide();
-      $("#celeb3").hide();
+
+     if (
+      ((side1 + side2) < side3 ) || ((side1 + side3) < side2 ) || ((side2 + side3) < side1 )
+    ) {
+      $("#notATriangle").show();
+    } else if ((side1 === side2) && (side1 === side3)) {
+      $("#equilateral").show();
+    } else if (
+      ((side1 === side2) && (side1 !== side3)) || ((side2 === side3) && (side2 !== side1)) || ((side1 === side3) && (side1 !== side2))
+    ) {
+      $("#isosceles").show();
+    } else if (
+      ((side1 !== side2) && (side2 !== side3) && (side1 !== side3))
+    ) {
+      $("#scalene").show();
     }
-    } else {
-      $("#error1").show();
+    else {
+      $("#notATriangle").show();
     }
-
-
 
     event.preventDefault();
   });
